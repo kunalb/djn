9f935d586ebd707d6b03eabd7d212ad10149ab5e
Kunal Bhalla <bhalla.kunal@gmail.com>
2026-04-05 13:31:57 -0700

    Actually run the full loop for generating agent responses

--- ego/src/ego/core.py
+++ ego/src/ego/core.py
@@ -3,9 +3,11 @@ Scratch attempt
 """
 
 import asyncio
+import io
 import json
 import os
 
+from contextlib import redirect_stdout, redirect_stderr
 from pprint import pprint
 
 from openai import AsyncOpenAI
@@ -54,29 +56,59 @@ class Ego:
     async def msg(self, name: str, msg: str):
         # Update this to actually handle a full loop of interactions
         self._context.record_msg(name, msg)
-        response = await self._agents[name].asend(msg)
-        self._context.record_response(name, response)
 
-        for item in response.output:
-            match item.type:
-                case "reasoning":
-                    print("<Reasoning>")
-                    for row in item.content:
-                        print(row.text, end="")
-                    print("</Reasoning>")
-                case "message":
-                    for row in item.content:
-                        print(row.text, end="")
-                case "function_call":
-                    args = json.loads(item.arguments)
-                    print("<Tool>")
-                    print(f"{item.name}(**{args})")
-                    print("</Tool>")
+        msgs = [{"role": "user", "content": msg}]
+        replies = []
 
-                    assert item.name == "exec"
-                    exec(args["source"], globals=globals(), locals={})
-                case _:
-                    pprint(item)
+        while msgs:
+            response = await self._agents[name].asend(msgs)
+            self._context.record_response(name, response)
+            msgs = []
+
+            print(f"<Status>{response.status}</Status>")
+            for item in response.output:
+                match item.type:
+                    case "reasoning":
+                        print("<Reasoning>")
+                        for row in item.content:
+                            print(row.text, end="")
+                        print("</Reasoning>")
+                    case "message":
+                        for row in item.content:
+                            print(row.text)
+                    case "function_call":
+                        args = json.loads(item.arguments)
+                        print("<Tool>")
+                        print(f"{item.name}(**{{")
+                        for k, v in args.items():
+                            print(f'  "{k}": "{v}"')
+                        print("}})")
+                        print("</Tool>")
+
+                        assert item.name == "exec"
+                        with redirect_stdout(io.StringIO()) as tool_stdout, redirect_stderr(io.StringIO()) as tool_stderr:
+                            try:
+                                exec(args["source"], globals=globals(), locals={})
+                            except Exception:
+                                ...
+
+                        if tool_stdout.getvalue():
+                            print("<StdOut>")
+                            print(tool_stdout.getvalue(), end="")
+                            print("</StdOut>")
+
+                        if tool_stderr.getvalue():
+                            print("<StdErr>")
+                            print(tool_stderr.getvalue(), end="")
+                            print("</StdErr>")
+
+                        msgs.append({
+                            "type": "function_call_output",
+                            "call_id": item.call_id,
+                            "output": "stdout: " + tool_stdout.getvalue() + "\nstderr: " + tool_stderr.getvalue(),
+                        })
+                    case _:
+                        pprint(item)
 
         return response
 
@@ -101,17 +133,20 @@ class Ego:
 async def agent(ego: Ego, model: str, client: AsyncOpenAI):
     response = None
     previous_response_id = None
-    msgs = []
+
+    all_msgs = []
 
     while True:
-        msg = yield response
-        msgs.append({"role": "user", "content": msg})
+        msgs = yield response
+        all_msgs.extend(msgs)
+        print("Calling agent...")
         response = await client.responses.create(
             instructions="""You are an agent running in a Python REPL.
 You can execute code to interact with the repl and get results.""",
             model=model,
-            input=msgs,
+            input=all_msgs,
             previous_response_id=previous_response_id,
             tools=await ego.list_tools(),
         )
         previous_response_id = response.id
+        all_msgs.extend(response.output)
